c42565416ada8c8d3dab70f9757f250a73ce247c
josh <josh@pekasys.com>
2021-11-30 00:43:40 -0600

    initial steps

--- tokio-chat/tokio-chat/src/main.rs
+++ tokio-chat/tokio-chat/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (socket, addr) = listener.accept().await.unwrap();
 }
