a013705e78d410c97a26549004ebc07872ba31d6
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-05 12:28:18 -0600

    fill tank with 2 random values. lock/unlock working

--- test-concurrent/src/main.rs
+++ test-concurrent/src/main.rs
@@ -1,8 +1,14 @@
-use futures::{stream, StreamExt}; // 0.3.5
-use reqwest::Client; // 0.10.6
-use tokio; // 0.2.21, features = ["macros"]
+use futures::{stream, StreamExt}; 
+use reqwest::Client;
+use tokio;
+// use crate::tokio::io::Error;
+use std::io::Error;
 use std::collections::VecDeque;
 use std::sync::Mutex;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 use std::any::type_name;
 
@@ -27,20 +33,44 @@ const CONCURRENT_REQUESTS: usize = 2;
 //     }
 // }
 
+#[derive(Deserialize, Debug)]
+struct Json {
+    #[serde(rename = "type")]
+    type_: String,
+    length: u32,
+    data: Vec<u32>,
+    success: bool,
+}
+
+
 #[tokio::main]
-async fn main() -> Result<(), ()> {  // -> Result<(), Box<dyn Error>> {
+async fn main()  -> Result<(), reqwest::Error> {  // -> Result<(), Box<dyn Error>> {
 
-    // let client = Client::new();
+    let client = Client::new();
+    let size_response = 1;
+    let request_url = format!("https://qrng.anu.edu.au/API/jsonI.php?length={}&type=uint16&size=12", size_response);
+    println!("{}", &request_url);
 
-    // let mut tank = Tank::new(); //Mutex::new(VecDeque::new());
-    let mut tank = Mutex::new(VecDeque::new());
-    // let mut tank: VecDeque<u32> = VecDeque::with_capacity(10);
+    // let mut tank = Tank::new();
+    // let mut tank = Mutex::new(VecDeque::new());
+    let mut tank = Mutex::new(VecDeque::with_capacity(10));
     
     {
+        let response = client.clone().get(&request_url).send().await?;
+        let result = response.json::<Json>().await?;
+        println!("{:?}", &result);
+        
         let mut tank = tank.lock().unwrap();
-        tank.push_back(3);
-        // tank.push_back(2);
-    // tank.push_back(3);
+        tank.push_back(result.data[0]);
+    }
+
+    {
+        let response = client.clone().get(&request_url).send().await?;
+        let result = response.json::<Json>().await?;
+        println!("{:?}", &result);
+        
+        let mut tank = tank.lock().unwrap();
+        tank.push_back(result.data[0]);
     }
 
 
