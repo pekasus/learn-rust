2b7e20ac2cfbf16bcd571e9c39e5285c2f8b8e9c
Joshua Meyer <josh.engineer9@gmail.com>
2021-12-07 00:39:16 -0600

    rename blockchain directory

---
+++ simple-blockchain/src/main.rs
@@ -0,0 +1,32 @@
+use blockchainlib::*;
+
+fn main () {
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, now(), vec![0; 32], 0,
+     "Genesis block!".to_owned(), difficulty);
+
+    block.mine();
+    println!("Mined genesis block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, now(), last_hash, 0,
+     "Another block!".to_owned(), difficulty);
+
+        block.mine();
+        println!("Mined block {:?}", &block);
+        
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
+    }
+}
